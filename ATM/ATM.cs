cc822c1bfc19276d0809d734545948347eb41439
K Waduge <89051451+waduge340@users.noreply.github.com>
2022-12-10 19:56:19 +0800

    initial commit

---
+++ ATM/ATM.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ATM
+{
+
+    class ATM
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the ATM");
+            Console.WriteLine("");
+            Console.WriteLine("Please Enter the Card Number: ");
+            string cardNumber = Console.ReadLine();
+            Console.WriteLine("Card Number is: "+cardNumber);
+
+            //Search Card Number in the system
+
+            Console.WriteLine("Please Enter the PIN: ");
+            string pinNumber = Console.ReadLine();
+            Console.WriteLine("PIN is: " + pinNumber);
+
+            //Check if the PIN is valid
+
+        }
+    }
+}
+
