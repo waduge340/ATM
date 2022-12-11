1b86762896a856892b7da6b5c23d5aa11ef74b77
K Waduge <89051451+waduge340@users.noreply.github.com>
2022-12-11 12:02:35 +0800

    second commit - validations

--- ATM/ATM.cs
+++ ATM/ATM.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace ATM
 {
@@ -7,21 +8,104 @@ namespace ATM
     {
         static void Main(string[] args)
         {
+            string[,] cardDetails = { { "1234567890", "1234" }, { "2468101214", "1793" }, { "3691215182", "7539" }, { "4812162024", "9517" } };
+            string pinNumber = ""; 
             Console.WriteLine("Welcome to the ATM");
             Console.WriteLine("");
+
             Console.WriteLine("Please Enter the Card Number: ");
             string cardNumber = Console.ReadLine();
-            Console.WriteLine("Card Number is: "+cardNumber);
+            Console.WriteLine("Card Number is: " + cardNumber);
 
-            //Search Card Number in the system
+            ATM obj = new ATM();
 
-            Console.WriteLine("Please Enter the PIN: ");
-            string pinNumber = Console.ReadLine();
-            Console.WriteLine("PIN is: " + pinNumber);
+            //obj.searchCard(cardNumber, cardDetails);
 
-            //Check if the PIN is valid
-
+            if (!(obj.searchCard(cardNumber, cardDetails)))
+            {
+                Console.WriteLine("Please Enter a valid card number");
+                Console.WriteLine("Good Bye!");
+            }
+            else
+            {
+                Console.WriteLine("Card Number is Valid");
+                Console.WriteLine("Please Enter the PIN: ");
+                pinNumber = Console.ReadLine();
+                Console.WriteLine("PIN is: " + pinNumber);
+                obj.validPIN(obj.pin(cardNumber, cardDetails), pinNumber);
+            }
         }
+
+        
+
+        public bool searchCard(string cardNumber, string[,] cardDetails) {
+
+            //string matchedCard;
+            bool isCardValid = false;
+
+            for (int i = 0; i < cardDetails.GetLength(0); i++)
+            {
+                if (cardDetails[i, 0] != cardNumber)
+                {
+                    isCardValid = false;
+                    continue;
+                }
+                else 
+                {
+                    isCardValid = true;
+                    break;
+                }
+            }
+            return isCardValid;
+        }
+
+        public string pin(string cardNumber, string[,] cardDetails)
+        {
+            string pin = "";
+            for (int i = 0; i < cardDetails.GetLength(0); i++)
+            {
+                if (cardDetails[i, 0] != cardNumber)
+                {
+                    continue;
+                }
+                else
+                {
+                    pin = cardDetails[i, 1];
+                    //Console.WriteLine("Correct PIN is: "+pin);
+                    break;
+                }
+            }
+            return pin;
+        }
+
+        public void validPIN(string pinNumber, string validPIN)
+        {
+            
+            if (pinNumber == validPIN) {
+                Console.WriteLine("PIN is Valid");
+            }
+            else
+            {
+                Console.WriteLine("Invalid PIN. Access Denied!");
+            }
+        }
+
+
+        
+
+        /*public bool matchPIN(string path, string cardNumber, string pinNumber)
+        {
+            if (!File.Exists(path))
+            {
+                using (StreamWriter sw = File.CreateText(path)) { }
+            }
+            else
+            {
+                using (StreamReader sr = File.OpenText(path)) { }
+                return true;
+            }
+            
+        }*/
     }
 }
 
