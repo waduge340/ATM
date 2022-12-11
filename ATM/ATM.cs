8e8e12b22dbaf3abd47f30bffe83e79df565224f
K Waduge <89051451+waduge340@users.noreply.github.com>
2022-12-11 13:22:37 +0800

    Commit Three - Files

--- ATM/ATM.cs
+++ ATM/ATM.cs
@@ -9,15 +9,19 @@ namespace ATM
         static void Main(string[] args)
         {
             string[,] cardDetails = { { "1234567890", "1234" }, { "2468101214", "1793" }, { "3691215182", "7539" }, { "4812162024", "9517" } };
-            string pinNumber = ""; 
+            string pinNumber = "";
+            string selection;
+            int selectionInt;
             Console.WriteLine("Welcome to the ATM");
             Console.WriteLine("");
 
             Console.WriteLine("Please Enter the Card Number: ");
             string cardNumber = Console.ReadLine();
-            Console.WriteLine("Card Number is: " + cardNumber);
+            //Console.WriteLine("Card Number is: " + cardNumber);
 
             ATM obj = new ATM();
+            //Console.WriteLine("hhhhhh");
+            obj.checkFiles(cardDetails);
 
             //obj.searchCard(cardNumber, cardDetails);
 
@@ -28,11 +32,21 @@ namespace ATM
             }
             else
             {
-                Console.WriteLine("Card Number is Valid");
-                Console.WriteLine("Please Enter the PIN: ");
+                //Console.WriteLine("Card Number is Valid");
+                Console.WriteLine("\nPlease Enter the PIN: ");
                 pinNumber = Console.ReadLine();
-                Console.WriteLine("PIN is: " + pinNumber);
-                obj.validPIN(obj.pin(cardNumber, cardDetails), pinNumber);
+                //Console.WriteLine("PIN is: " + pinNumber);
+                if (!(obj.validPIN(obj.pin(cardNumber, cardDetails), pinNumber))) {
+                    Console.WriteLine("Invalid PIN. Access Denied!");
+                }
+                else
+                {
+                    Console.WriteLine("\nSelect one of the options\n");
+                    Console.Write("1. Check Balance \n2. Withraw Money \n3. Deposit Money \n4. Change PIN \n");
+                    selection = Console.ReadLine();
+                    selectionInt = Convert.ToInt32(selection);
+                }
+
             }
         }
 
@@ -78,29 +92,56 @@ namespace ATM
             return pin;
         }
 
-        public void validPIN(string pinNumber, string validPIN)
+        public bool validPIN(string pinNumber, string validPIN)
         {
+            bool isPinValid = false;
             
             if (pinNumber == validPIN) {
-                Console.WriteLine("PIN is Valid");
+                //Console.WriteLine("PIN is Valid");
+                isPinValid= true;
             }
             else
             {
-                Console.WriteLine("Invalid PIN. Access Denied!");
+                isPinValid= false;
+            }
+            return isPinValid;
+        }
+
+        public void customerSelection(int selectionInt)
+        {
+            switch (selectionInt)
+            {
+                case 1:
+                    Console.WriteLine("you selected 1");
+                    break;
+                case 2:
+                    Console.WriteLine("you selected 2");
+                    break;
+                case 3: 
+                    Console.WriteLine("you selected 3");
+                    break;
+                case 4: 
+                    Console.WriteLine("you slected 4");
+                    break;
+                default: 
+                    Console.WriteLine("Invalid Selection");
+                    break;
             }
         }
 
-
-        
-
-        /*public bool matchPIN(string path, string cardNumber, string pinNumber)
+        public void checkFiles(string[,] cardDetails)
         {
-            if (!File.Exists(path))
+            for (int i = 0; i < cardDetails.GetLength(0); i++)
             {
-                using (StreamWriter sw = File.CreateText(path)) { }
+                string path = @"C:\Users\kavit\source\repos\ATM\ATM\Cards\";
+
+                // Create the file if it does not exist.
+                if (!File.Exists(path + cardDetails[i,0] + ".txt"))
+                {
+                    File.Create(path + cardDetails[i, 0] + ".txt");
+                }
             }
-            }
-            
+        }
     }
 }
 
